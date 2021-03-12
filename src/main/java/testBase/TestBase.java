f5db54a292b5c460129ec23334539c628e1b273b
Amit Dhankhar <amit.dhankhar@ticketmaster.com>
2021-03-12 21:55:20 +0530

    removed wait

--- src/main/java/testBase/TestBase.java
+++ src/main/java/testBase/TestBase.java
@@ -31,7 +31,7 @@ public class TestBase extends ObjectRepo{
 		    driver = new FirefoxDriver(options);
 		}
 		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get(PropertiesOperations.getPropertyValue("url"));
 	}
